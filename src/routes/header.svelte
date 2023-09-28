8c3abed58d80370853db3183560d5133c0970120
Mohamed Abdillahi <mahamadamiin140@gmail.com>
2023-09-28 14:32:53 +0300

    fix: header links

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -2,11 +2,11 @@
 	<nav class="h-16 flex justify-between items-center text-[#fafaf9]">
 		<h4 class="font-semibold">FITNESS PORTFOLIO</h4>
 		<ul class="flex gap-4">
-			<li><a href="/workouts">Workouts</a></li>
-			<li><a href="/programs">Programs</a></li>
-			<li><a href="/community">Community</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/store">Store</a></li>
+			<li><a href="#/">Workouts</a></li>
+			<li><a href="#/">Programs</a></li>
+			<li><a href="#/">Community</a></li>
+			<li><a href="#/">About</a></li>
+			<li><a href="#/">Store</a></li>
 		</ul>
 	</nav>
 </header>
